aecb696429ea9fb6a3bd5d60acc89094df535294
ND-SR7 <davogic.sr7.2021@uns.ac.rs>
2024-07-19 16:00:43 +0200

    Updated AI and version numbers
    - Updated AI move generation. Value tables are now fixed and CPU side is taken into account
    - Updated version number to 1.1.0

--- client/src/services/client/PieceSortService.tsx
+++ client/src/services/client/PieceSortService.tsx
@@ -2,7 +2,7 @@ import Field from "../../models/Field/Field";
 
 export const whiteSideSort = (a: Field, b: Field) => {
   if (a.row !== b.row) {
-      return b.row - a.row;
+    return b.row - a.row;
   }
   return a.column.localeCompare(b.column);
 };
