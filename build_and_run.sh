f7eb3bd9aff05103784099ef06cea21e0a119e43
ND-SR7 <ndavogic3@gmail.com>
2025-04-07 18:14:10 +0200

    Updated build_and_run script for better UX

--- build_and_run.sh
+++ build_and_run.sh
@@ -15,41 +15,40 @@ cleanup() {
 
 trap cleanup SIGINT
 
-log_message "Navigating to /client directory..."
-cd client || { log_message "Failed to navigate to /client directory."; exit 1; }
-
-log_message "Running npm install to install node_modules..."
-npm install || { log_message "npm install failed."; exit 1; }
-
-log_message "Starting the client app..."
-npm start &> ../logs/client_log.txt &
-CLIENT_PID=$!
-if [ $? -eq 0 ]; then
-  log_message "Client app started successfully (PID: $CLIENT_PID). Waiting for the page to load..."
-  while ! curl -sSf http://localhost:3000 &> /dev/null; do
-    sleep 5
-  done
-  log_message "Client app is up and running at http://localhost:3000."
-else
-  log_message "Failed to start the client app."
-  exit 1
-fi
-
-log_message "Navigating to /server directory..."
-cd ..
-cd server || { log_message "Failed to navigate to /server directory."; exit 1; }
+log_message "Navigating to server directory..."
+cd server || { log_message "Failed to navigate to server directory."; exit 1; }
 
 log_message "Starting the server app using Spring Boot..."
-./mvnw spring-boot:run &> ../logs/server_log.txt &
+./mvnw spring-boot:run &> ../logs/server.log &
 SERVER_PID=$!
 if [ $? -eq 0 ]; then
-  log_message "Server app started successfully (PID: $SERVER_PID). Waiting for Spring Boot to initialize..."
+  log_message "Server process started successfully (PID: $SERVER_PID). Waiting for Spring Boot to initialize..."
   while ! curl -sSf http://localhost:8080/api/v1/ping &> /dev/null; do
     sleep 5
   done
   log_message "Server app is up and running at http://localhost:8080."
 else
   log_message "Failed to start the server app."
+  exit 1
+fi
+
+log_message "Navigating to client directory..."
+cd ../client || { log_message "Failed to navigate to client directory."; exit 1; }
+
+log_message "Running npm install to install node_modules..."
+npm install || { log_message "npm install failed."; exit 1; }
+
+log_message "Starting the client app..."
+npm start &> ../logs/client.log &
+CLIENT_PID=$!
+if [ $? -eq 0 ]; then
+  log_message "Client process started successfully (PID: $CLIENT_PID). Waiting for the page to load..."
+  while ! curl -sSf http://localhost:3000 &> /dev/null; do
+    sleep 5
+  done
+  log_message "Client app is up and running at http://localhost:3000."
+else
+  log_message "Failed to start the client app."
   exit 1
 fi
 
