8a21377dc9e4f233e5861066b39fa5f2ef0068a1
ND-SR7 <ndavogic3@gmail.com>
2025-06-15 17:22:20 +0200

    Added previous move visualization

--- client/src/models/Field/Field.tsx
+++ client/src/models/Field/Field.tsx
@@ -2,8 +2,10 @@ import Piece from "../Piece/Piece";
 
 export type ColumnString = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H";
 type RowNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+type ColorString = "aliceblue" | "lightblue";
 
 type Field = {
+  color: ColorString;
   row: RowNumber;
   column: ColumnString;
   piece?: Piece;
--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -38,23 +38,23 @@ interface GamePageProps {
 
 const GamePage = ({gameType} : GamePageProps) => {
   const [fields, setFields] = useState<Field[]>([
-    {row: 8, column: "A"}, {row: 8, column: "B"}, {row: 8, column: "C"}, {row: 8, column: "D"},
-    {row: 8, column: "E"}, {row: 8, column: "F"}, {row: 8, column: "G"}, {row: 8, column: "H"},
-    {row: 7, column: "A"}, {row: 7, column: "B"}, {row: 7, column: "C"}, {row: 7, column: "D"},
-    {row: 7, column: "E"}, {row: 7, column: "F"}, {row: 7, column: "G"}, {row: 7, column: "H"},
-    {row: 6, column: "A"}, {row: 6, column: "B"}, {row: 6, column: "C"}, {row: 6, column: "D"},
-    {row: 6, column: "E"}, {row: 6, column: "F"}, {row: 6, column: "G"}, {row: 6, column: "H"},
-    {row: 5, column: "A"}, {row: 5, column: "B"}, {row: 5, column: "C"}, {row: 5, column: "D"},
-    {row: 5, column: "E"}, {row: 5, column: "F"}, {row: 5, column: "G"}, {row: 5, column: "H"},
-    {row: 4, column: "A"}, {row: 4, column: "B"}, {row: 4, column: "C"}, {row: 4, column: "D"},
-    {row: 4, column: "E"}, {row: 4, column: "F"}, {row: 4, column: "G"}, {row: 4, column: "H"},
-    {row: 3, column: "A"}, {row: 3, column: "B"}, {row: 3, column: "C"}, {row: 3, column: "D"},
-    {row: 3, column: "E"}, {row: 3, column: "F"}, {row: 3, column: "G"}, {row: 3, column: "H"},
-    {row: 2, column: "A"}, {row: 2, column: "B"}, {row: 2, column: "C"}, {row: 2, column: "D"},
-    {row: 2, column: "E"}, {row: 2, column: "F"}, {row: 2, column: "G"}, {row: 2, column: "H"},
-    {row: 1, column: "A"}, {row: 1, column: "B"}, {row: 1, column: "C"}, {row: 1, column: "D"},
-    {row: 1, column: "E"}, {row: 1, column: "F"}, {row: 1, column: "G"}, {row: 1, column: "H"}
-  ]);
+  { row: 8, column: "A", color: "aliceblue" }, { row: 8, column: "B", color: "lightblue" }, { row: 8, column: "C", color: "aliceblue" }, { row: 8, column: "D", color: "lightblue" },
+  { row: 8, column: "E", color: "aliceblue" }, { row: 8, column: "F", color: "lightblue" }, { row: 8, column: "G", color: "aliceblue" }, { row: 8, column: "H", color: "lightblue" },
+  { row: 7, column: "A", color: "lightblue" }, { row: 7, column: "B", color: "aliceblue" }, { row: 7, column: "C", color: "lightblue" }, { row: 7, column: "D", color: "aliceblue" },
+  { row: 7, column: "E", color: "lightblue" }, { row: 7, column: "F", color: "aliceblue" }, { row: 7, column: "G", color: "lightblue" }, { row: 7, column: "H", color: "aliceblue" },
+  { row: 6, column: "A", color: "aliceblue" }, { row: 6, column: "B", color: "lightblue" }, { row: 6, column: "C", color: "aliceblue" }, { row: 6, column: "D", color: "lightblue" },
+  { row: 6, column: "E", color: "aliceblue" }, { row: 6, column: "F", color: "lightblue" }, { row: 6, column: "G", color: "aliceblue" }, { row: 6, column: "H", color: "lightblue" },
+  { row: 5, column: "A", color: "lightblue" }, { row: 5, column: "B", color: "aliceblue" }, { row: 5, column: "C", color: "lightblue" }, { row: 5, column: "D", color: "aliceblue" },
+  { row: 5, column: "E", color: "lightblue" }, { row: 5, column: "F", color: "aliceblue" }, { row: 5, column: "G", color: "lightblue" }, { row: 5, column: "H", color: "aliceblue" },
+  { row: 4, column: "A", color: "aliceblue" }, { row: 4, column: "B", color: "lightblue" }, { row: 4, column: "C", color: "aliceblue" }, { row: 4, column: "D", color: "lightblue" },
+  { row: 4, column: "E", color: "aliceblue" }, { row: 4, column: "F", color: "lightblue" }, { row: 4, column: "G", color: "aliceblue" }, { row: 4, column: "H", color: "lightblue" },
+  { row: 3, column: "A", color: "lightblue" }, { row: 3, column: "B", color: "aliceblue" }, { row: 3, column: "C", color: "lightblue" }, { row: 3, column: "D", color: "aliceblue" },
+  { row: 3, column: "E", color: "lightblue" }, { row: 3, column: "F", color: "aliceblue" }, { row: 3, column: "G", color: "lightblue" }, { row: 3, column: "H", color: "aliceblue" },
+  { row: 2, column: "A", color: "aliceblue" }, { row: 2, column: "B", color: "lightblue" }, { row: 2, column: "C", color: "aliceblue" }, { row: 2, column: "D", color: "lightblue" },
+  { row: 2, column: "E", color: "aliceblue" }, { row: 2, column: "F", color: "lightblue" }, { row: 2, column: "G", color: "aliceblue" }, { row: 2, column: "H", color: "lightblue" },
+  { row: 1, column: "A", color: "lightblue" }, { row: 1, column: "B", color: "aliceblue" }, { row: 1, column: "C", color: "lightblue" }, { row: 1, column: "D", color: "aliceblue" },
+  { row: 1, column: "E", color: "lightblue" }, { row: 1, column: "F", color: "aliceblue" }, { row: 1, column: "G", color: "lightblue" }, { row: 1, column: "H", color: "aliceblue" }
+]);
 
   const pieces: Piece[] = [
     { id: "rw1", imgSrc: rookWhite, FEN: "R", PGN: "R" },
@@ -225,6 +225,17 @@ const GamePage = ({gameType} : GamePageProps) => {
       openModal();
     } else {
       syncPgnAfterPromote();
+    }
+
+    if (lastMove !== undefined) { // visualizing previous move
+      fields.forEach(field => {
+        document.getElementById(fieldToString(field))!.style.backgroundColor = field.color;
+      });
+
+      const fromFieldDiv = document.getElementById(fieldToString(fields[lastMove.from]));
+      const toFieldDiv = document.getElementById(fieldToString(fields[lastMove.to]));
+      fromFieldDiv!.style.backgroundColor = "yellowgreen";
+      toFieldDiv!.style.backgroundColor = "greenyellow";
     }
   }, [fields, syncPgnAfterPromote, gameType]);
 
